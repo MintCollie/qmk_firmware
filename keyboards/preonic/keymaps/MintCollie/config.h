d133fe6024995c8c487f66081ca38d65936c4184
Todd McIntire <mint@Todds-MacBook-Pro.local>
2021-11-02 08:28:23 -0500

    setting custom sounds

--- keyboards/preonic/keymaps/MintCollie/config.h
+++ keyboards/preonic/keymaps/MintCollie/config.h
@@ -17,11 +17,13 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PREONIC_SOUND)
+#    define STARTUP_SONG SONG(MY_SONG)
+//
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+        { SONG(MY_SONG), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+//
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
