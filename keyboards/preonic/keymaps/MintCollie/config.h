8f9e76981efe6cfd1db403b3ed54bb794b65e592
Todd McIntire <mint@Todds-MacBook-Pro.local>
2021-11-03 11:53:15 -0500

    fixe audio startup issue

--- keyboards/preonic/keymaps/MintCollie/config.h
+++ keyboards/preonic/keymaps/MintCollie/config.h
@@ -17,7 +17,8 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(STARTUP_SOUND)
+    #define AUDIO_INIT_DELAY
+    #define STARTUP_SONG SONG(STARTUP_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
